296b7402e0447d109c0c4402240223dc89f024cc
Patchmo <72225271+Patch-mo@users.noreply.github.com>
2023-01-31 14:04:25 +0000

    changed return text in js

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
-const InputElement = document.getElementById('inputNumber');
-const buttonElement = document.getElementById('calcBtn');
+const earningsElement = document.getElementById('inputEarnings');
+const earningsButtonElement = document.getElementById('calcEarningsBtn');
 const taxElement = document.getElementById('resultTax');
 const saveElement = document.getElementById('resultSave');
 const payElement = document.getElementById('resultPay');
 const keepElement = document.getElementById('resultKeep');
 
-const calculator = () => {
-    let income = InputElement.value;
+const costsCalculator = () => {
+    let income = earningsElement.value;
     let tax = income * (20 / 100);
-    taxElement.textContent = `Save £${tax} for income tax`;
+    taxElement.textContent = `Put £${tax} aside for income tax`;
     let save = income * (20 / 100);
-    saveElement.textContent = `Save £${save} in a savings account for the future`;
+    saveElement.textContent = `Save £${save} for the future`;
     let pay = income * (20 / 100);
     payElement.textContent = `Pay yourself £${pay}`;
     let keep = income * (40 / 100);
-    keepElement.textContent = `Keep £${keep} in your business account`;
+    keepElement.textContent = `Keep £${keep} in your business account for day to day business costs`;
 };
 
-buttonElement.addEventListener('click', calculator);
-
-if (!buttonElement) {
-    console.error('button element not found');
-}
+earningsButtonElement.addEventListener('click', costsCalculator);
